2ef5a131a16fa7460e0f04b6c72edab5e89b2543
Brandon Montgomery <bmonty@gmail.com>
2012-01-21 13:49:06 -0500

    allow process to define port

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  var app, coffeeDir, express, publicDir, routes;
+  var app, coffeeDir, express, port, publicDir, routes;
   // Module dependencies.
   express = require('express');
   routes = require('./routes');
@@ -28,6 +28,7 @@
   });
   // Routes
   app.get('/', routes.index);
-  app.listen(3000);
+  port = process.env.port || 3000;
+  app.listen(port);
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 })();
