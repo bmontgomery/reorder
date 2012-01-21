77c04e69d78a86381bc311bd539e294e3d345561
Brandon Montgomery <bmonty@gmail.com>
2012-01-21 12:58:10 -0500

    get js files in there for deployment to heroku

---
+++ app.js
@@ -0,0 +1,33 @@
+(function(){
+  var app, coffeeDir, express, publicDir, routes;
+  // Module dependencies.
+  express = require('express');
+  routes = require('./routes');
+  app = (module.exports = express.createServer());
+  coffeeDir = __dirname + '/coffee';
+  publicDir = __dirname + '/public';
+  // Configuration
+  app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(require('connect-assets')());
+    return app.use(express.static(publicDir));
+  });
+  app.configure('development', function() {
+    app.use(express.logger());
+    return app.use(express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+    }));
+  });
+  app.configure('production', function() {
+    return app.use(express.errorHandler());
+  });
+  // Routes
+  app.get('/', routes.index);
+  app.listen(3000);
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+})();
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+(function(){
+  // GET home page.
+  exports.index = function(req, res) {
+    return res.render('index', {
+      title: 'Reorder',
+      scripts: ['https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js', 'http://ajax.aspnetcdn.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js']
+    });
+  };
+})();
