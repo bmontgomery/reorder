5a08c9fcc0f000c67e1e3891768c3cbf5d42dcb6
Brandon Montgomery <bmonty@gmail.com>
2012-01-22 00:31:27 -0500

    initialize ui on loaded list

--- routes/index.coffee
+++ routes/index.coffee
@@ -4,10 +4,10 @@ listProvider = new ListProvider()
 # GET home page.
 
 exports.index = ( req, res ) ->
-	# TODO: if req.params.id is populated, load up the list; otherwise, use a new list
 	viewData = {
 		title: 'Reorder'
 		scripts: ['https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js','https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js','http://ajax.aspnetcdn.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js']
+		item: '' 
 	}
 
 	if req.params.id and req.params.id.match "^[A-Z]+$"
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@
   // GET home page.
   exports.index = function(req, res) {
     var viewData;
-    // TODO: if req.params.id is populated, load up the list; otherwise, use a new list
     viewData = {
       title: 'Reorder',
-      scripts: ['https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js', 'http://ajax.aspnetcdn.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js']
+      scripts: ['https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js', 'http://ajax.aspnetcdn.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js'],
+      item: ''
     };
     if (req.params.id && req.params.id.match("^[A-Z]+$")) {
       return listProvider.findByTextID(req.params.id, function(x, r) {
